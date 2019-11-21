afc24b08630a885bd1bd41e2ba5391d8e212b4a2
git <brooksby@ge.com>
2019-11-21 09:31:39 -0500

    Made the script tolerant to starting with a different number of command line arguments.

--- python/forever.py
+++ python/forever.py
@@ -2,8 +2,17 @@
 from subprocess import Popen
 import sys
 
-prog_name = sys.argv[1]
-arg_val = sys.argv[2]
+prog_name = ""
+arg_val = ""
+if len(sys.argv) == 1:
+    print("No script to run was specified. Exiting.")
+    exit()
+
+if len(sys.argv) > 1:
+    prog_name = sys.argv[1]
+
+if len(sys.argv) > 2:
+    arg_val = sys.argv[2]
 
 while True:
     execute_str = prog_name + " " + arg_val
