6bee951d4d2e46da57c0b442caf93f0fc9a30721
Glen Brooksby <brooksby@ge.com>
2020-07-13 11:18:01 -0400

    Changed order of two commands in the mastering process.  This seems to fix the problem we were having with mastering.

--- mobile_app/platforms/ios/e4PtTool/Plugins/IFC242xPlugin/CDVIFC242x.m
+++ mobile_app/platforms/ios/e4PtTool/Plugins/IFC242xPlugin/CDVIFC242x.m
@@ -817,8 +817,8 @@ enum ifc242xValue {
     [self.plugin.commandDelegate sendPluginResult:result callbackId:self.plugin.cmd.callbackId];
     [self.telnetCmds addObject:[NSString stringWithFormat:@"MASTERSIGNAL 01DIST1 NONE\n"]];
     [self.telnetCmds addObject:[NSString stringWithFormat:@"MASTERSIGNAL 01DIST1 5.0\n"]];
-    [self.telnetCmds addObject:[NSString stringWithFormat:@"MASTER 01DIST1 SET\n"]];
     [self.telnetCmds addObject:[NSString stringWithFormat:@"MASTER 01DIST1 ACTIVE\n"]];
+    [self.telnetCmds addObject:[NSString stringWithFormat:@"MASTER 01DIST1 SET\n"]];
     [self sendTelnetCommand];
 }
 
