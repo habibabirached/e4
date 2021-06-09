45dd0db65b828192800b3cdd4fd3132b71335d04
HabibAbirached <abirached@ge.com>
2021-06-09 13:57:30 -0700

    adding a trash can icon

--- mobile_app/platforms/ios/www/js/e4pt_app.js
+++ mobile_app/platforms/ios/www/js/e4pt_app.js
@@ -2591,7 +2591,8 @@ define(function(require, exports, module) {
             cell0.setAttribute("nativeURL",entries[i].nativeURL);
 
             var cell1 = new_row.insertCell(-1);
-            cell1.innerHTML = 'delete entry';
+            // '<i class="bi bi-trash"></i>'  //  '<i class="fa fa-trash-o" style="font-size:36px"></i>';// 'delete entry';
+            cell1.innerHTML =  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>'
             cell1.setAttribute("onclick",deleteEntry);
             //cell1.setAttribute("nativeURL",entries[i].nativeURL);
             rowIdx += 1;
