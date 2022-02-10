ee46bce9929a610ebea353157404233dd78fd1c7
Katlic, Peter Leigh <katlic@ge.com>
2022-02-10 13:12:32 -0500

    Revert "Fix log file export (#131)" (#138)
    This reverts commit aba9b1e407fb6c0243f9c88e0a66b464e356a802.

--- mobile_app/platforms/ios/www/js/e4pt_app.js
+++ mobile_app/platforms/ios/www/js/e4pt_app.js
@@ -50,8 +50,6 @@ define(function(require, exports, module) {
     var CALC_METHOD_NEW = 2;
     var CALC_METHOD_NONE = 3;
     
-    var MAX_LOG_MESSAGES = 10;
-    
     var CUSTOMER_REPORT_FILE_PREFIX = "customer_report_e4Pt";
     var DETAILS_FILE_PREFIX = "details_e4pt";
     var EMAIL_FILE_PREFIX = "e4pt";
@@ -750,9 +748,8 @@ define(function(require, exports, module) {
     
     function edit_save() {
         console.log("@edit_save");
-        editModal.hide();
         // changing frame will erase data
-        if (document.getElementById("FRAME_SIZE_EDIT").value !== document.getElementById("FRAME_SIZE").value) {
+        if (document.getElementById("FRAME_SIZE_EDIT").value != document.getElementById("FRAME_SIZE").value) {
             document.getElementById("FRAME_SIZE").value = document.getElementById("FRAME_SIZE_EDIT").value;
             document.getElementById("FRAME_SIZE").selectedIndex = document.getElementById("FRAME_SIZE_EDIT").selectedIndex;
             document.getElementById("HEADER_FRAME").innerHTML = document.getElementById("FRAME_SIZE_EDIT").value;
@@ -762,32 +759,32 @@ define(function(require, exports, module) {
             send_scan_metadata(true, true);
             turbine_setup();
         }
-        if (document.getElementById("SERIAL_NUMBER_EDIT").value !== document.getElementById("SERIAL_NUMBER").value) {
+        if (document.getElementById("SERIAL_NUMBER_EDIT").value != document.getElementById("SERIAL_NUMBER").value) {
             document.getElementById("SERIAL_NUMBER").value = document.getElementById("SERIAL_NUMBER_EDIT").value;
             document.getElementById("HEADER_SERIAL").innerHTML = document.getElementById("SERIAL_NUMBER_EDIT").value;
             send_scan_metadata(true, true);
         }
-        if (document.getElementById("CUSTOMER_EDIT").value !== document.getElementById("CUSTOMER").value) {
+        if (document.getElementById("CUSTOMER_EDIT").value != document.getElementById("CUSTOMER").value) {
             document.getElementById("CUSTOMER").value = document.getElementById("CUSTOMER_EDIT").value;
             document.getElementById("HEADER_CUSTOMER").innerHTML = document.getElementById("CUSTOMER_EDIT").value;
             send_scan_metadata(true, true);
         }
-        if (document.getElementById("SITE_EDIT").value !== document.getElementById("SITE").value) {
+        if (document.getElementById("SITE_EDIT").value != document.getElementById("SITE").value) {
             document.getElementById("SITE").value = document.getElementById("SITE_EDIT").value;
             document.getElementById("HEADER_SITE").innerHTML = document.getElementById("SITE_EDIT").value;
             send_scan_metadata(true, true);
         }
-        if (document.getElementById("OPERATOR_EDIT").value !== document.getElementById("OPERATOR").value) {
+        if (document.getElementById("OPERATOR_EDIT").value != document.getElementById("OPERATOR").value) {
             document.getElementById("OPERATOR").value = document.getElementById("OPERATOR_EDIT").value;
             document.getElementById("HEADER_SITE").innerHTML = document.getElementById("OPERATOR_EDIT").value;
             send_scan_metadata(true, true);
         }
-        if (document.getElementById("UNITS_EDIT").value !== document.getElementById("UNITS").value) {
+        if (document.getElementById("UNITS_EDIT").value != document.getElementById("UNITS").value) {
             document.getElementById("UNITS").value = document.getElementById("UNITS_EDIT").value;
             document.getElementById("HEADER_UNITS").innerHTML = document.getElementById("UNITS_EDIT").value;
             send_scan_metadata(true, true);
         }
-        send_scan_metadata(false, true);
+        editModal.hide();
         $("#TURBINE_SETUP_PAGE").fadeIn();
     }
 
@@ -847,15 +844,11 @@ define(function(require, exports, module) {
     }
     
     function showLog() {
-        $("#PROCESSING_PAGE").fadeIn();
-        $("#LOG_MESSAGE_COUNT").text(logMessages.length);
-        var displayCount = Math.min(logMessages.length, MAX_LOG_MESSAGES);
-        $("#MAX_MESSAGE_COUNT").text(displayCount);
         var elementID = "LOG_TABLE_BODY";
         var prev_tbody = document.getElementById(elementID);
         var tbody = document.createElement("tbody");
         tbody.setAttribute("id",elementID);
-        for (var i = displayCount - 1; i >= 0; i--) {
+        for (var i = logMessages.length - 1; i >= 0; i--) {
             var new_row = tbody.insertRow(-1);
             
             var cell1 = new_row.insertCell(-1);
@@ -865,10 +858,8 @@ define(function(require, exports, module) {
             cell3.innerHTML = logMessages[i].message;
         }
         prev_tbody.parentNode.replaceChild(tbody, prev_tbody);
-        setTimeout(function() {
-            $("#PROCESSING_PAGE").fadeOut();
-            logModal.show();
-        }, 500);
+        
+        logModal.show();
     }
     
     function clearLog() {
@@ -877,7 +868,6 @@ define(function(require, exports, module) {
     }
     
     function exportLog() {
-        $("#PROCESSING_PAGE").fadeIn();
         let contents = "";
         for (let i=0; i<logMessages.length; i++) {
             contents += logMessages[i].timestamp + ": " + logMessages[i].message;
@@ -886,10 +876,8 @@ define(function(require, exports, module) {
         let targetFolder = "data"; // default directory
         let logDate = new Date().toJSON().slice(0, 19).replace(/-/g,'').replace(/:/g,'').replace('T','');
         let fileName = LOG_FILE_PREFIX + "_" + logDate + ".txt";
-        let fileUrl = cordova.file.documentsDirectory + targetFolder + "/" + fileName;
         writeToFile(targetFolder, fileName, contents, function() {
-            $("#PROCESSING_PAGE").fadeOut();
-            fileDownloadFunction(fileUrl);
+            fileDownloadFunction(fileName);
         });
     }
     
@@ -1326,7 +1314,6 @@ define(function(require, exports, module) {
     }
 
     function send_scan_metadata(reset, updatingExisting = false) {
-      console.log('@send_scan_metadata');
       // Set element suffix if we are updating existing measurement
       var suffix = (updatingExisting ? '_EDIT' : '');
 
@@ -1962,7 +1949,7 @@ define(function(require, exports, module) {
     }
 
     function writeToFile(folder, fileName, fileData, callback=null) {
-        console.log("@writeToFile: folder=" + folder + ", fileName=" + fileName);
+        console.log("@writeToFile: fileName = ", fileName);
         var targetFolder = cordova.file.documentsDirectory + folder + "/";
         window.resolveLocalFileSystemURL(targetFolder, function(dir) {
             dir.getFile(fileName, {create:true, exclusive: false}, function(file) {
@@ -2154,7 +2141,7 @@ define(function(require, exports, module) {
           if ((casing_thickness <= max) && (casing_thickness >= min)) {
             for(var j=0; j<spacers[i].position.length; j++) {
               if (position == spacers[i].position[j]) {
-                console.log(JSON.stringify(spacers[i]));
+                console.log(spacers[i]);
                 spacer = {'size':spacers[i].size, 'color':spacers[i].color, 'image':spacers[i].image};
                 current_stage_type = spacers[i].stage;
                 spacer_found = true;
@@ -2675,12 +2662,9 @@ define(function(require, exports, module) {
 
                     if (fromCalculateRPM) {
                         console.log('calculating RPM');
-                        // TODO: already set in calculateRPM function
                         position = document.getElementById("SENSOR_POSITION").value;
                         casing_thickness = document.getElementById("CURR_CASE_THICKNESS").value;
                         if (position && casing_thickness) {
-                            console.log('position:', position);
-                            console.log('casing_thickness:', casing_thickness);
                             stage_details = get_stage_details(position, casing_thickness);
                             console.log(stage_details);
                             let rotor_blades = stage_details.blade_count;
